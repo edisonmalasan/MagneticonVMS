b96c223c0b1e9086477de3f70ee2e570982d9eed
Eruki <malasanedison1@gmail.com>
2025-04-24 01:30:29 +0800

    done with register fxml

--- src/App/controller/RegisterController.java
+++ src/App/controller/RegisterController.java
@@ -1,4 +1,37 @@
 package App.controller;
 
-public class RegisterController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterController implements Initializable {
+
+    @FXML
+    private ComboBox<String> sexComboBox;
+
+    @FXML
+    private ComboBox<String> roleComboBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeComboBox();
+    }
+
+    private void initializeComboBox(){
+        setSexComboBox();
+        setRoleComboBox();
+    }
+
+    private void setSexComboBox() {
+        sexComboBox.getItems().addAll("Male", "Female", "Other");
+    }
+
+    private void setRoleComboBox() {
+        roleComboBox.getItems().addAll("Member", "Admin");
+    }
+
+
 }
