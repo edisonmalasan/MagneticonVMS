a569a06920287571822b2cc6dc107a0d71766a78
Eruki <malasanedison1@gmail.com>
2025-04-24 01:49:40 +0800

    changes in login fxml and added components in controller

--- src/App/controller/LoginController.java
+++ src/App/controller/LoginController.java
@@ -1,4 +1,18 @@
 package App.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private TextField loginEmail;
+
+    @FXML
+    private PasswordField loginPassword;
+
+    @FXML
+    private Hyperlink registerLink;
 }
