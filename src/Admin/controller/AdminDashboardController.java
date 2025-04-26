0bb4c58e1602e798b625a83491bafbf489c4d88d
Edison Malasan <2245504@slu.edu.ph>
2025-04-26 11:04:44 +0800

    id

--- src/Admin/controller/AdminDashboardController.java
+++ src/Admin/controller/AdminDashboardController.java
@@ -1,12 +1,12 @@
 package Admin.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 
 public class AdminDashboardController {
 
-
-
+    private Button logoutButton;
 
     @FXML
     private void handleLogout() {
