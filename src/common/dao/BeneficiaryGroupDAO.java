89ad1a860b06affbcf4ab019d07aa5fef47a920d
Yuen, Ka Yue Ymanuelle T. <2235608@slu.edu.ph>
2025-04-26 19:17:33 +0800

    Add "mysql-connector-j-9.2.0" into "lib" directory Added contents in DAOs

--- src/common/dao/BeneficiaryGroupDAO.java
+++ src/common/dao/BeneficiaryGroupDAO.java
@@ -1,4 +1,26 @@
 package common.dao;
 
+import common.models.BeneficiaryGroup;
+import common.utils.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class BeneficiaryGroupDAO {
+
+    public boolean createBeneficiaryGroup(BeneficiaryGroup group) {
+        String sql = "INSERT INTO BENEFICIARY_GROUPS (benid, bengroup, bendesc) VALUES (?, ?, ?)";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, group.getBenid());
+            statement.setString(2, group.getBengroup());
+            statement.setString(3, group.getBendesc());
+            int rowsInserted = statement.executeUpdate();
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- src/common/dao/VolunteerTeamDAO.java
+++ src/common/dao/VolunteerTeamDAO.java
@@ -1,5 +1,34 @@
 package common.dao;
 
+import common.models.Team;
+import common.utils.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class VolunteerTeamDAO {
     // TODO: Query
+    public List<Team> getAllVolunteerTeams() {
+        String sql = "SELECT * FROM VOLUNTEER_TEAM";
+        List<Team> teams = new ArrayList<>();
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet rs = statement.executeQuery()) {
+            while (rs.next()) {
+                Team team = new Team();
+                team.setTeamid(rs.getString("teamid"));
+                team.setTname(rs.getString("teamname"));
+                team.setTdesc(rs.getString("tdesc"));
+                teams.add(team);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return teams;
+    }
+
 }
