4e33c35d0e1d59b43daf3206ad4b05df9c33f7fc
Personal <2240879@slu.edu.ph>
2025-05-01 19:05:50 +0800

    Modified volunteer dao

--- src/common/dao/VolunteerDAO.java
+++ src/common/dao/VolunteerDAO.java
@@ -3,10 +3,7 @@ package common.dao;
 import common.models.Volunteer;
 import common.utils.DatabaseConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,19 +36,24 @@ public class VolunteerDAO {
         return null;
     }
 
-    public boolean createVolunteer(Volunteer volunteer) {
-        String sql = "INSERT INTO Volunteer (volid, fname, lname, email, password, role) " +
-                "VALUES (?, ?, ?, ?, ?)";
+    public static boolean createVolunteer(Volunteer volunteer) {
+        String sql = "INSERT INTO Volunteer (volid, lname, fname, address, phone, email, password, bday, sex, volstat, role) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         try (Connection connection = DatabaseConnection.getConnection();
         PreparedStatement statement = connection.prepareStatement(sql)) {
 
             statement.setString(1, volunteer.getVolid());
-            statement.setString(2, volunteer.getFname());
-            statement.setString(3, volunteer.getLname());
-            statement.setString(4, volunteer.getEmail());
-            statement.setString(5, volunteer.getPassword());
-            statement.setString(6, volunteer.getRole());
+            statement.setString(2, volunteer.getLname());
+            statement.setString(3, volunteer.getFname());
+            statement.setString(4, volunteer.getAddress());
+            statement.setString(5, volunteer.getPhone());
+            statement.setString(6, volunteer.getEmail());
+            statement.setString(7, volunteer.getPassword());
+            statement.setDate(8, Date.valueOf(volunteer.getBirthday()));
+            statement.setString(9, volunteer.getSex());
+            statement.setString(10, volunteer.getVolstat());
+            statement.setString(11, volunteer.getRole());
 
             return statement.executeUpdate() > 0;
         } catch (SQLException e) {
