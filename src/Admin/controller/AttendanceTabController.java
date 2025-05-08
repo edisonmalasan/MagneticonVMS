ed21b08aa665e83972a83c8f59731337a6aff99e
Eruki <malasanedison1@gmail.com>
2025-05-08 18:25:50 +0800

    added skeleton for attendance tab

--- src/Admin/controller/AttendanceTabController.java
+++ src/Admin/controller/AttendanceTabController.java
@@ -1,4 +1,72 @@
 package Admin.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class AttendanceTabController {
+
+    @FXML
+    private TableColumn<?, ?> attendanceIdColumn;
+
+    @FXML
+    private TableView<?> attendanceTable;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private TableColumn<?, ?> dateColumn;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TableColumn<?, ?> serviceColumn;
+
+    @FXML
+    private ComboBox<?> serviceComboBox;
+
+    @FXML
+    private TableColumn<?, ?> statusColumn;
+
+    @FXML
+    private ComboBox<?> statusComboBox;
+
+    @FXML
+    private TableColumn<?, ?> timeInColumn;
+
+    @FXML
+    private TextField timeInField;
+
+    @FXML
+    private TableColumn<?, ?> timeOutColumn;
+
+    @FXML
+    private TextField timeOutField;
+
+    @FXML
+    private TableColumn<?, ?> volunteerColumn;
+
+    @FXML
+    private ComboBox<?> volunteerComboBox;
+
+    @FXML
+    void handleClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSave(ActionEvent event) {
+
+    }
+
 }
